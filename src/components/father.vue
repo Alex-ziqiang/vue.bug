5f5150f48943805a7549ce291ab2507ebafefa07
“ziqiang_duan@139.com” <ziqiang_duan@139.com>
2019-07-08 11:56:08 +0800

    add

---
+++ src/components/father.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="demo">
+        <button @click="del">删除全部</button>
+        <demo :myarr="arr"></demo>
+    </div>
+</template>
+<script>
+    import demo from './sun'
+  export default {
+      data(){
+          return {
+              arr:[]
+          }
+      },
+      components:{demo},
+      methods:{
+          del(){
+              this.arr=[];
+          },
+      }
+  }
+</script>
+<style>
+    .demo {
+        width: 800px;
+        height: 600px;
+        margin: 100px auto;
+        border: 1px solid #E5B98C;
+    }
+</style>
---
+++ src/components/sun.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <button @click="change">改变</button>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                arr:[]
+            }
+        },
+        props:['myarr'],
+        watch:{
+            myarr:{
+                handler:function(val){
+                  // debugger;
+                    if(val.length===0){
+                        this.arr = val
+                        window.console.log(this.arr)
+                    }else {
+                        this.arr.push('12')
+                        window.console.log(this.arr)
+                    }
+
+                },
+                deep:true
+            }
+        },
+        methods:{
+          change(){
+              this.arr.push('init');
+          }
+        }
+    }
+</script>
